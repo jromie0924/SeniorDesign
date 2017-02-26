9c07f9dc372c5634a474bdfc73b9e48412a2c89e
Jackson <jromie0924@gmail.com>
2017-02-26 17:32:26 -0500

    data separation makes a row of 42 features for each image (that being 14 haralick features for each channel of each image). Then it also returns a list of classes, each associated with its index in the training features matrix. This as a whole is the training data.

--- separateData.m
+++ separateData.m
@@ -1,16 +1,21 @@
-function [trainData features] = separateData(data)
-    length = size(data);
-    
-    for a = 1:length(3)
-        glcms = data(:,:,a);
-        glcmRed = glcms{1};
-        glcmGreen = glcms{3};
-        glcmBlue = glcms{5};
-        featuresRed = glcms{2};
-        featuresGreen = glcms{4};
-        featuresBlue = glcms{6};
-        
-        trainData(:,:,a) = {glcmRed glcmGreen glcmBlue};
-        features(:,:,a) = {featuresRed featuresGreen featuresBlue};
+function [features, classes] = separateData(allData)
+    counter = 1;
+    length = size(allData);
+    for a = 1:length(2)
+        data = allData(a);
+        data = data{1};
+        currentLength = size(data);
+        for b = 1:currentLength(3)
+            featureData = data(:,:,b);
+            featuresRed = featureData{2};
+            featuresGreen = featureData{4};
+            featuresBlue = featureData{6};
+            
+            allFeatures = horzcat(featuresRed.', featuresGreen.', featuresBlue.');
+            features(counter,:) = allFeatures;
+            
+            classes(counter) = a;
+            counter = counter + 1;
+        end
     end
 end
